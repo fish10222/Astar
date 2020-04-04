aab0fce93d1130d5fe4ce902f1ca6b82128b67d2
Alex Nguyen <hmn5@sfu.ca>
2020-04-04 09:38:31 -0700

    simple ID, add normal astar

--- Astar.py
+++ Astar.py
@@ -1,7 +1,38 @@
 import time as timer
-from single_agent_planner import compute_heuristics, get_sum_of_cost
+from single_agent_planner import compute_heuristics, get_sum_of_cost, a_star, get_location
 
 from Astar_coupled import a_star_coupled
+
+def detect_collisions(paths):
+    # Return a list of first collisions between all robot pairs.
+
+    collisions = list()
+    for i in range(len(paths) - 1):
+        for j in range(i + 1, len(paths)):
+            collision = detect_collision(paths[i], paths[j])
+            if collision:
+                collision['a1'] = i
+                collision['a2'] = j
+                collisions.append(collision)
+    return collisions
+
+def detect_collision(path1, path2):
+    # Return the first collision that occurs between two robot paths (or None if there is no collision)
+
+    for i in range(max(len(path1), len(path2))):
+        loc1 = get_location(path1, i)
+        loc2 = get_location(path2, i)
+        if loc1 == loc2:
+            return {'loc': [loc1], 'timestep': i}
+        if i > 0 and loc1 == get_location(path2, i-1) and loc2 == get_location(path1, i-1):
+            return {'loc': [loc1, loc2], 'timestep': i}
+    return None
+
+def merge_group(group_list, g1, g2, agentList):
+    group_list[g1] = group_list[g1] + group_list[g2]
+    for i in group_list[g1]:
+        agentList[i] = g1
+    return group_list[g1]
 
 class AstarSolver(object):
     """A planner that plans for each robot independently."""
@@ -28,8 +59,47 @@ class AstarSolver(object):
         """ Finds paths for all agents from their start locations to their goal locations."""
 
         start_time = timer.time()     
-        result = a_star_coupled(self.my_map, self.starts, self.goals, self.heuristics, [])
+        
+        result = []
+        agentList = [i for i in range(self.num_of_agents)]     
+        groupList = [[i] for i in range(self.num_of_agents)]      
 
+        # for i in range(self.num_of_agents):  # Find initial path for each agent
+        #     path = a_star(self.my_map, self.starts[i], self.goals[i], self.heuristics[i],
+        #                   i, [])
+        #     if path is None:
+        #         raise BaseException('No solutions')
+        #     result.append(path)
+
+        # collisions = detect_collisions(result)
+
+        # while collisions:
+        #     a1 = collisions[0]['a1']
+        #     a2 = collisions[0]['a2']
+
+        #     conflict_group = merge_group(groupList, agentList[a1], agentList[a2], agentList)
+            
+        #     print("NEW")
+        #     print(groupList)
+        #     print(agentList)
+        #     start_locs = [self.starts[i] for i in conflict_group]
+        #     goal_locs = [self.goals[i] for i in conflict_group]
+        #     heuristics = [self.heuristics[i] for i in conflict_group]
+        #     print(start_locs)
+        #     print(goal_locs)
+
+            
+        #     new_paths = a_star_coupled(self.my_map, start_locs, goal_locs, heuristics, [])
+        #     print("NEW PATH")
+        #     print(new_paths)
+        #     print()
+        #     print()
+        #     for i in range(len(conflict_group)):
+        #         result[conflict_group[i]] = new_paths[i]
+
+        #     collisions = detect_collisions(result)
+            
+        result = a_star_coupled(self.my_map, self.starts, self.goals, self.heuristics, [])
 
         self.CPU_time = timer.time() - start_time
 
--- single_agent_planner.py
+++ single_agent_planner.py
@@ -1,7 +1,7 @@
 import heapq
 
 def move(loc, dir):
-    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0), (0, 0)]
     return loc[0] + directions[dir][0], loc[1] + directions[dir][1]
 
 
@@ -54,8 +54,32 @@ def build_constraint_table(constraints, agent):
     #               for a more efficient constraint violation check in the 
     #               is_constrained function.
 
-    pass
+    # constraints are indexed by timestep
 
+    negConstraintTbl = dict()
+    posConstraintTbl = dict()
+    for constraint in constraints:        
+        # skip unrelated constraints
+        if constraint['agent'] != agent:
+            continue
+
+        agentTS = constraint['timestep']
+        locList = constraint['loc']
+
+        constraintTbl = negConstraintTbl if 'positive' not in constraint or not constraint['positive'] else posConstraintTbl
+
+        if agentTS not in constraintTbl:
+            constraintTbl[agentTS] = set()
+        
+        # Vertex constraint
+        if len(locList) == 1:
+            constraintTbl[agentTS].add(locList[0])
+        
+        # Edge constraint
+        elif len(locList) == 2:
+            constraintTbl[agentTS].add(locList[0] + locList[1])
+
+    return negConstraintTbl, posConstraintTbl
 
 def get_location(path, time):
     if time < 0:
@@ -81,9 +105,19 @@ def is_constrained(curr_loc, next_loc, next_time, constraint_table):
     # Task 1.2/1.3: Check if a move from curr_loc to next_loc at time step next_time violates
     #               any given constraint. For efficiency the constraints are indexed in a constraint_table
     #               by time step, see build_constraint_table.
+    
+    if 'goals' in constraint_table:
+        if next_loc in constraint_table['goals']:
+            return True
+    
+    if next_time not in constraint_table:
+        return False
+    
+    edgeConstraint = curr_loc + next_loc
 
-    pass
-
+    if (next_loc in constraint_table[next_time]) or (edgeConstraint in constraint_table[next_time]):
+        return True
+    return False
 
 def push_node(open_list, node):
     heapq.heappush(open_list, (node['g_val'] + node['h_val'], node['h_val'], node['loc'], node))
@@ -110,34 +144,70 @@ def a_star(my_map, start_loc, goal_loc, h_values, agent, constraints):
     ##############################
     # Task 1.1: Extend the A* search to search in the space-time domain
     #           rather than space domain, only.
+
+    constraintTable, posConstraintTbl = build_constraint_table(constraints, agent)
+
     open_list = []
     closed_list = dict()
-    earliest_goal_timestep = 0
+
+    intKey = []
+    if 'goals' in constraintTable:
+        for x in constraintTable.keys():
+            if x != "goals": intKey.append(x)
+    else:
+        intKey = constraintTable.keys()
+    earliest_goal_timestep = max(intKey) if len(intKey) != 0 else 0
+    
+    upperBound = len(my_map) * len(my_map[0])
     h_value = h_values[start_loc]
-    root = {'loc': start_loc, 'g_val': 0, 'h_val': h_value, 'parent': None}
+    
+    # Adding timestep to root and child variables (1.1.1)
+    root = {'loc': start_loc, 'g_val': 0, 'h_val': h_value, 'parent': None, 'timestep': 0}
     push_node(open_list, root)
-    closed_list[(root['loc'])] = root
+
+    # Expand closed list to use (cell, timestep) as index (1.1.2)
+    closed_list[(root['loc'], root['timestep'])] = root
     while len(open_list) > 0:
         curr = pop_node(open_list)
         #############################
         # Task 1.4: Adjust the goal test condition to handle goal constraints
-        if curr['loc'] == goal_loc:
+        if curr['loc'] == goal_loc and curr['timestep'] >= earliest_goal_timestep:
             return get_path(curr)
-        for dir in range(4):
+        
+        if curr['timestep'] > upperBound:
+            break
+        
+        for dir in range(5):
             child_loc = move(curr['loc'], dir)
+
+            # Prune out of range move (since test map doesnt have @)
+            if child_loc[0] < 0 or child_loc[1] < 0 or  child_loc[0] > len(my_map) - 1 or  child_loc[1] > len(my_map[0]) - 1:
+                continue
+            
             if my_map[child_loc[0]][child_loc[1]]:
                 continue
+
+            # Prune constrained move
+            if is_constrained(curr['loc'], child_loc, curr['timestep'] + 1, constraintTable):
+                continue
+
+            # Prune positive constrained move
+            if curr['timestep'] + 1 in posConstraintTbl:
+                if not is_constrained(curr['loc'], child_loc, curr['timestep'] + 1, posConstraintTbl):
+                    continue
+
             child = {'loc': child_loc,
                     'g_val': curr['g_val'] + 1,
                     'h_val': h_values[child_loc],
-                    'parent': curr}
-            if (child['loc']) in closed_list:
-                existing_node = closed_list[(child['loc'])]
+                    'parent': curr,
+                    'timestep': curr['timestep'] + 1}
+            if (child['loc'], child['timestep']) in closed_list:
+                existing_node = closed_list[(child['loc'], child['timestep'])]
                 if compare_nodes(child, existing_node):
-                    closed_list[(child['loc'])] = child
+                    closed_list[(child['loc'], child['timestep'])] = child
                     push_node(open_list, child)
             else:
-                closed_list[(child['loc'])] = child
+                closed_list[(child['loc'], child['timestep'])] = child
                 push_node(open_list, child)
 
     return None  # Failed to find solutions
